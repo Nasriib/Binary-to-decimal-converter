a590bd9f3149151139d933775e09d417170adec6
Nasri Ibrahim <99497859+Nasriib@users.noreply.github.com>
2022-04-16 13:43:36 -0400

    Create bin.h

---
+++ bin.h
@@ -0,0 +1,12 @@
+#ifndef ISIP_EXAMPLE
+#define ISIP_EXAMPLE
+//include files
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//function being implemented in to program
+//
+double binary_to_decimal(char* str);
+
+#endif
